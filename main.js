6274c9d9dd8ebd20620dae16f4f2b8721e993147
Markese Straughter <Kese08@hotmail.com>
2020-03-23 22:06:56 -0700

    added color

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
 
-//let count = 1
-//var id = setInterval(() => {
-//$('#seconds').html(count)
-//count++
-
-//if (count >= 61){
-   // clearInterval(id)
-//}
-//},1000)
-
-// i need to call a function after 60 secs, 60 mins, 24hrs
-
-
-
 
 
 function clockWork(){
@@ -40,26 +26,5 @@ function clockWork(){
 
 
 
-//function clockWork() {
-  //var date = new Date(); /* creating object of Date class */
-  //var hour = date.getHours();
-  //var min = date.getMinutes();
-  //var sec = date.getSeconds();
-  //hour = updateTime(hour);
-  //min = updateTime(min);
-  //sec = updateTime(sec);
-  //document.getElementById("practice").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
-    //var t = setInterval(function(){ currentTime() }, 1000); /* setting timer */
-//}
 
-//function updateTime(k) {
-  //if (k < 10) {
-    //return "0" + k;
-  //}
-  //else {
-    //return k;
-  //}
-//}
-
-//clockWork(); //* calling currentTime() function to 
 
